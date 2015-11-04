4fbb6879f7294cf0269be0c683bfcec74075509e
Tammo Jan Dijkema <dijkema@astron.nl>
2015-11-04 15:16:59 +0000

    Task #8422: polish libcpp improvements
    git-svn-id: https://svn.astron.nl/LOFAR/branches/LOFAR-libcpp-Task8422/CEP/PyBDSM@32784 b3281354-443f-0410-9bd6-bf5212d75c11

--- src/c++/MGFunction2.cc
+++ src/c++/MGFunction2.cc
@@ -34,10 +34,16 @@ and parameters under exponents (NL_ij) are non-linear.
 
 #include "boost_python.h"
 #include "MGFunction.h"
-    
+   
+#if not(defined(_LIBCPP_VERSION))
+#include <ext/algorithm>
+#endif
 #include <num_util/num_util.h>
 #include <cfloat>
 
+#if not(defined(_LIBCPP_VERSION))
+using namespace __gnu_cxx;
+#endif
 using namespace std;
 namespace n = num_util;
 
