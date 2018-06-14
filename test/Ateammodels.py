36164f7ad7565b1a0817f7ca6f0590bb593406fb
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2018-06-14 22:51:25 +0200

    Rename variables that used reserved names

--- test/Ateammodels.py
+++ test/Ateammodels.py
@@ -16,8 +16,8 @@ from bdsf.analysis import plotresults
 """ Try blindly running bdsf to see if boxsize is ok, so fitting doesnt hang. Then try various segmenting algorithms which dont
 depend on rms ? """
 
-dir = "A-team/"
-ls = subprocess.Popen(["ls",dir], stdout=subprocess.PIPE).communicate()[0]
+directory = "A-team/"
+ls = subprocess.Popen(["ls",directory], stdout=subprocess.PIPE).communicate()[0]
 ls = ls.split('\n')
 
 files = []; rmsbox = []
@@ -25,13 +25,13 @@ chain = [Op_loadFITS(), Op_collapse(), Op_preprocess(), Op_rmsimage(), Op_thresh
 
 #ls = ['subim.fits']
 bms = [(0.0015, 0.0015, 0.0)]
-dir=''
-for ifile, file in enumerate(ls):
-  op = subprocess.Popen(["file",dir+file], stdout=subprocess.PIPE).communicate()[0]
+directory=''
+for ifile, filename in enumerate(ls):
+  op = subprocess.Popen(["file",directory+filename], stdout=subprocess.PIPE).communicate()[0]
   if "FITS image data" in op:
-    print 'Processing ', file
-    img = bdsf.execute(chain, {'fits_name': file, 'thresh':"hard", 'solnname' : 'new', 'beam' : bms[ifile]}), 'indir' : dir})
-    files.append(file)
+    print 'Processing ', filename
+    img = bdsf.execute(chain, {'fits_name': filename, 'thresh':"hard", 'solnname' : 'new', 'beam' : bms[ifile]}), 'indir' : directory})
+    files.append(filename)
     rmsbox.append(img.opts.rms_box)
 
     thr = img.clipped_rms
@@ -44,10 +44,10 @@ for ifile, file in enumerate(ls):
     pl.subplot(2,2,2); pl.imshow(N.transpose(op1), origin='lower', interpolation='nearest'), pl.title('watershed1')
     pl.subplot(2,2,3); pl.imshow(N.transpose(markers1), origin='lower', interpolation='nearest'), pl.title('markers1')
     pl.subplot(2,2,4); plotresults(img, newfig=False, cbar=False)
-    pl.savefig(dir+file+'_watershed.png')
+    pl.savefig(directory+filename+'_watershed.png')
 
   else:
-    print dir+file+' is not a FITS file !!'
+    print directory+filename+' is not a FITS file !!'
 
 
 
