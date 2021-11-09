1859a71beeed9e902f3b063d1272d43e12337d61
Marcel Loose <loose@astron.nl>
2021-11-09 10:35:37 +0100

    Buid binary wheels for Python 3.8 and 3.9
    Added support for building binary wheels for Python 3.8 and 3.9.
    Removed support for Python 2.7, 3.4 and 3.5, because these version
    are obsolete and no longer supported by manylinux2010.

--- manylinux2010/build.sh
+++ manylinux2010/build.sh
@@ -3,7 +3,7 @@
 HERE=`dirname "$0"`
 cd $HERE/..
 
-for i in 37 27 34 35 36; do
+for i in 36 37 38 39; do
     docker build -f manylinux2010/wheel${i}.docker . -t pybdsf${i}
     docker run -v `pwd`/manylinux2010:/manylinux2010 pybdsf${i} sh -c "cp /output/*.whl /manylinux2010/."
 done
