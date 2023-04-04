4abab9b28f3cab052081ae1b11a9e05e2ac2c98f
Marcel Loose <loose@astron.nl>
2023-04-04 12:31:25 +0200

    WIP: cibuildwheel; macos add CFLAGS and LDFLAGS

--- cibuildwheel/build_boost.sh
+++ cibuildwheel/build_boost.sh
@@ -6,6 +6,12 @@ PYUNICODE=$([ ${PYMAJOR} -eq 3 -a ${PYMINOR} -le 7 ] && echo "m" || echo "")
 TARGET=cp${PYMAJOR}${PYMINOR}-cp${PYMAJOR}${PYMINOR}${PYUNICODE}
 THREADS=$(python -c 'import multiprocessing as mp; print(mp.cpu_count())')
 
+CFLAGS="${CFLAGS} $(python-config --cflags)"
+LDFLAGS=${LDFLAGS} $(python-config --ldflags)"
+
+echo "===> CFLAGS=${CFLAGS} <==="
+echo "===> LDFLAGS=${LDFLAGS} <==="
+
 # rm -rf /build
 # mkdir /build
 cd ${BOOST_BUILD_DIR}/boost
