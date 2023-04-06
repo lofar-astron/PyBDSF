161ffe7372cc99ca58fa0ccd4a57e4ae5ab40d3e
Marcel Loose <loose@astron.nl>
2023-04-06 17:18:44 +0200

    WIP: cibuildwheel; install numpy before build

--- cibuildwheel/download_boost.sh
+++ cibuildwheel/download_boost.sh
@@ -8,8 +8,8 @@ BOOST_PATCH=$(echo ${BOOST_VERSION} | cut -d. -f3)
 
 NAME="boost"
 LONG_NAME="${NAME}_${BOOST_MAJOR}_${BOOST_MINOR}_${BOOST_PATCH}"
-BASE_URL="https://ufpr.dl.sourceforge.net"
-DIRECTORY="project/${NAME}/${NAME}/${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}"
+BASE_URL="https://sourceforge.net"
+DIRECTORY="projects/${NAME}/files/${NAME}/${BOOST_MAJOR}.${BOOST_MINOR}.${BOOST_PATCH}"
 FILE="${LONG_NAME}.tar.bz2"
 
 URL="${BASE_URL}/${DIRECTORY}/${FILE}"
@@ -17,5 +17,5 @@ URL="${BASE_URL}/${DIRECTORY}/${FILE}"
 rm -rf ${BOOST_BUILD_DIR}
 mkdir -p ${BOOST_BUILD_DIR}
 cd ${BOOST_BUILD_DIR}
-curl -o - ${URL} | tar -xjf -
+curl -L -o - ${URL} | tar -xjf -
 mv ${LONG_NAME} ${NAME}
