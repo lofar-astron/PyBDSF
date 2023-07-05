a48cdebd4a4db69ae54e42c6ad0274e9695d4828
David Rafferty <drafferty@hs.uni-hamburg.de>
2023-07-05 13:49:43 +0200

    Restore import of NArray

--- bdsf/opts.py
+++ bdsf/opts.py
@@ -34,7 +34,7 @@ Opts.my_new_opt = Float(33, doc="docstring")
 from __future__ import absolute_import
 
 from .tc import Int, Float, Bool, String, Tuple, Enum, \
-    Option, Instance, tInstance, List, Any, TCInit, tcError
+    Option, NArray, Instance, tInstance, List, Any, TCInit, tcError
 
 try:
     # For Python 2
