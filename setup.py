f87dc1ccdf2c9431233e5f9afff055c39629693a
Marcel Loose <loose@astron.nl>
2024-05-03 17:43:01 +0200

    WIP: Move (almost) all project settings from setup.py to pyproject.toml
    All project settings have been moved from `setup.py` to `pyproject.toml`, except for a few that need to be set in the call to `setup()` in `setup.py`, as required by `scikit-build`.

--- setup.py
+++ setup.py
@@ -1,44 +1,6 @@
-from skbuild import setup  # This line replaces 'from setuptools import setup'
-# from setuptools_scm import get_version
+from skbuild import setup
 
 setup(
-    # name='bdsf',
-    # version=get_version(),
-    author='David Rafferty',
-    author_email='drafferty@hs.uni-hamburg.de',
-    url='https://github.com/lofar-astron/PyBDSF',
-    description='Blob Detection and Source Finder',
-    long_description=open('README.rst', 'rt').read(),
-    long_description_content_type='text/x-rst',
-    platforms='Linux, Mac OS X',
     packages=['bdsf', 'bdsf.nat'],
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'Programming Language :: C++',
-        'Programming Language :: Fortran',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Scientific/Engineering :: Astronomy'
-    ],
-    extras_require={
-        'ishell': [
-            'ipython!=8.11.*,!=8.12.*,!=8.13.*,!=8.14.*,!=8.15.*',
-            'matplotlib',
-        ],
-    },
-    install_requires=['backports.shutil_get_terminal_size',
-                        'astropy', 'numpy', 'scipy'],
-    python_requires=">=3.7",
-    entry_points = {
-        'console_scripts': [
-            'pybdsf = bdsf.pybdsf:main [ishell]',
-            'pybdsm = bdsf.pybdsf:main [ishell]'
-        ]
-    },
-    zip_safe=False,
     include_package_data=False,
 )
