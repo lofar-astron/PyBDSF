92ca70290792ddbc279d13cb07aaae91558b66e3
Marcel Loose <loose@astron.nl>
2024-05-03 17:55:41 +0200

    WIP: Add comment
    Added a comment in `setup.py`, explaining why we need to pass some configuration options as arguments to `setup()`, instead of providing them in `pyproject.toml`.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from skbuild import setup
 
+# Scikit-build requires some options to be passed in the call to `setup()`.
+# Hence, these cannot be set in `pyproject.toml`
 setup(
-    packages=['bdsf', 'bdsf.nat'],
+    packages=["bdsf", "bdsf.nat"],
     include_package_data=False,
 )
