924668681169e3fdee751c389b118a8d817fd78b
Marcel Loose <loose@astron.nl>
2024-05-06 10:24:53 +0200

    Rap 614 use setuptools scm (#224)
    * Exclude all files/directories from the source dist first, then include what we want. This is needed, because `setuptools` when used in combination with `setuptools_scm` adds all the files under revision control in the source distribution. We do not want that.
    
    * WIP: Add setuptools_scm
    
    Only adding `setuptools_scm` already required changes to `MANIFEST.in` to avoid that all the files under version control are put into the source distribution.
    TODO:
      * Actually start using automatic versioning
      * Move all project settings from `setup.py` to `pyproject.toml`
    
    * WIP: Rename _version.py to _changelog.py
    
    Make room for automatically generated `_version.py` file by renaming the existing file.
    
    * WIP: More or less working version
    
    This still needs some significant cleanup, but at least this appears to work.
    
    * WIP: Remove name and version from setup.py
    
    Automatic versioning only works if version is _not_ also set in `setup.py`.
    Also added `bdsf/_version.py` to the `.gitignore` file, because is should *never* be put under version control anymore.
    
    * WIP: Move (almost) all project settings from setup.py to pyproject.toml
    
    All project settings have been moved from `setup.py` to `pyproject.toml`, except for a few that need to be set in the call to `setup()` in `setup.py`, as required by `scikit-build`.
    
    * WIP: Add comment
    
    Added a comment in `setup.py`, explaining why we need to pass some configuration options as arguments to `setup()`, instead of providing them in `pyproject.toml`.
    
    * Strip trailing white-space

--- setup.py
+++ setup.py
@@ -1,42 +1,8 @@
-from skbuild import setup  # This line replaces 'from setuptools import setup'
+from skbuild import setup
 
+# Scikit-build requires some options to be passed in the call to `setup()`.
+# Hence, these cannot be set in `pyproject.toml`
 setup(
-    name='bdsf',
-    version='1.11.0a1',
-    author='David Rafferty',
-    author_email='drafferty@hs.uni-hamburg.de',
-    url='https://github.com/lofar-astron/PyBDSF',
-    description='Blob Detection and Source Finder',
-    long_description=open('README.rst', 'rt').read(),
-    long_description_content_type='text/x-rst',
-    platforms='Linux, Mac OS X',
-    packages=['bdsf', 'bdsf.nat'],
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'Programming Language :: C++',
-        'Programming Language :: Fortran',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Scientific/Engineering :: Astronomy'
-    ],
-    extras_require={
-        'ishell': [
-            'ipython!=8.11.*,!=8.12.*,!=8.13.*,!=8.14.*,!=8.15.*',
-            'matplotlib',
-        ],
-    },
-    install_requires=['backports.shutil_get_terminal_size',
-                        'astropy', 'numpy', 'scipy'],
-    python_requires=">=3.7",
-    entry_points = {
-        'console_scripts': [
-            'pybdsf = bdsf.pybdsf:main [ishell]',
-            'pybdsm = bdsf.pybdsf:main [ishell]'
-        ]
-    },
-    zip_safe=False,
+    packages=["bdsf", "bdsf.nat"],
+    include_package_data=False,
 )
