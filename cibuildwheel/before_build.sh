87383e7c8d3ec75d353f0dfb53f1d028b17c36af
Marcel Loose <loose@astron.nl>
2024-08-30 16:25:42 +0200

    Use Clang instead of GCC

--- cibuildwheel/before_build.sh
+++ cibuildwheel/before_build.sh
@@ -27,7 +27,9 @@ function build_boost_python
   nproc=$(python -c 'import multiprocessing as mp; print(mp.cpu_count())')
   inc_dir=$(python -c 'import sysconfig as sc; print(sc.get_path("include"))')
   cd "${BOOST_BUILD_DIR}/boost"
-  ./bootstrap.sh --prefix="${BOOST_INSTALL_DIR}" \
+  ./bootstrap.sh \
+    --prefix="${BOOST_INSTALL_DIR}" \
+    --with-toolset=clang \
     --with-libraries=python
   ./b2 -j"${nproc}" \
     cxxflags="-fPIC -I${inc_dir}" \
