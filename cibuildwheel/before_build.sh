6033d572e1edc442dce5cb5773b939f6b5f532fd
Marcel Loose <loose@astron.nl>
2024-09-04 16:52:30 +0200

    Use latest Numpy 1.x.y instead of oldest supported.

--- cibuildwheel/before_build.sh
+++ cibuildwheel/before_build.sh
@@ -2,8 +2,8 @@
 #
 # This script should be called by `cibuildwheel` in the `before-build` stage.
 #
-# This script will first install the oldest supported `numpy` to maximize
-# portability. Next the Boost Python libraries will be built from source,
+# This script will first install the latest `numpy` 1.x version (2.x is not
+# supported yet). Next the Boost Python libraries will be built from source,
 # including the bindings to NumPy. The Boost sources must be in the directory
 # `${BOOST_BUILD_DIR}/boost`. The libraries will be installed in the directory
 # `${BOOST_INSTALL_DIR}`. Both environment variables must have been set.
