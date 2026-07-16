f57490476cf791a65659dfff1ef5fcd22f07fdf0
Aleksander Kurek <aleksander.kurek@uj.edu.pl>
2026-07-16 09:40:40 +0200

    Fix `total_fluxE` computation (#301)
    * Fix `total_fluxE` computation
    
    * Remove duplicates
    
    Removed redundant calculations for total_flux and pixels_in_isl.

--- bdsf/islands.py
+++ bdsf/islands.py
@@ -353,7 +353,7 @@ class Island(object):
         valid_pixels = ~self.mask_active & ~N.isnan(self.image) # pixels that are both: inside the island and not NaN
         self.total_flux = N.nansum(self.image[valid_pixels])/beamarea
         pixels_in_isl = N.sum(~N.isnan(self.image[~self.mask_active]))  # number of unmasked pixels assigned to current island
-        self.total_fluxE = func.nanmean(bbox_rms_im[in_bbox_and_unmasked]) * N.sqrt(pixels_in_isl/beamarea)  # Jy
+        self.total_fluxE = func.nanmean(bbox_rms_im[valid_pixels]) * N.sqrt(pixels_in_isl/beamarea)  # Jy
         self.border = self.get_border()
         self.gaul = []
         self.fgaul = []
