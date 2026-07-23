c7de8fcf15e3fc9894e479e9bce2b6647c8e1862
Aleksander Kurek <aleksander.kurek@uj.edu.pl>
2026-07-23 13:44:38 +0200

    Fix external mask in `list_and_sort_gaussians` (#331)
    * Fix external mask in `list_and_sort_gaussians`
    
    * Revert reorder

--- bdsf/output.py
+++ bdsf/output.py
@@ -927,7 +927,9 @@ def list_and_sort_gaussians(img, patch=None, root=None,
                 gausflux = []
                 gausindx = []
             if use_mask:
-                patchnums.append(mask_labels[g.centre_pix[0], g.centre_pix[1]])
+                x_pix = round(g.centre_pix[0])
+                y_pix = round(g.centre_pix[1])
+                patchnums.append(mask_labels[x_pix, y_pix])
 
         if patch == 'source':
             sorted_gauslist = list(gauslist)
