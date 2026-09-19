1ef2c19f6213869a1171138deedd16fa1256d3d0
AlexKurek <aleksander.kurek@uj.edu.pl>
2026-09-19 17:24:09 +0200

    Fix Gaussian deconvolution error handling in `deconv2`

--- bdsf/functions.py
+++ bdsf/functions.py
@@ -522,7 +522,7 @@ def deconv2(gaus_bm, gaus_c):
             bmaj = sqrt(0.5*(s+t))
             bpa = rad * 0.5 * atan2(-gamma, alpha-beta)
         bmin = 0.0
-        if 0.5*(s-t) < limit and alpha > -limit and beta > -limit:
+        if 0.5*(s-t) < -limit and alpha > -limit and beta > -limit:
             ifail = 1
         else:
             ifail = 2
