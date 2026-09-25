1bb1848a95b3d615363fd56287935bc0435df623
AlexKurek <aleksander.kurek@uj.edu.pl>
2026-09-25 19:19:48 +0200

    Remove unneccesary variables

--- bdsf/gausfit.py
+++ bdsf/gausfit.py
@@ -570,7 +570,7 @@ class Op_gausfit(Op):
         mask = isl.mask_active
         inipeak, iniposn, im1 = func.get_maxima(im, mask, thr_pos, isl.shape, beam, im_pos=im_pos)
         if len(inipeak) == 0:
-            av, stdnew, maxv, maxp, minv, minp = func.arrstatmask(im, mask)
+            _, stdnew, maxv, maxp, _, _ = func.arrstatmask(im, mask)
             inipeak = [maxv]
             iniposn = [maxp]
         nmulsrc1 = len(iniposn)
@@ -578,7 +578,7 @@ class Op_gausfit(Op):
         domore = True
         while domore:
             domore = False
-            av, stdnew, maxv, maxp, minv, minp = func.arrstatmask(im1, mask)
+            _, stdnew, maxv, maxp, _, _ = func.arrstatmask(im1, mask)
             if stdnew > isl.rms and maxv >= thr and maxv >= isl.mean+2.0*isl.rms:
                 domore = True
                 x1, y1 = N.array(iniposn).transpose()
@@ -632,12 +632,11 @@ class Op_gausfit(Op):
             im_pos = im
             thr_pos = -1e9
         mask = isl.mask_active
-        av = img.clipped_mean
-        inipeak, iniposn, im1 = func.get_maxima(im, mask, thr_pos, isl.shape, beam, im_pos=im_pos)
+        _, iniposn, _ = func.get_maxima(im, mask, thr_pos, isl.shape, beam, im_pos=im_pos)
         npeak = len(iniposn)
         gaul = []
 
-        av, stdnew, maxv, maxp, minv, minp = func.arrstatmask(im, mask)
+        _, _, maxv, _, _, _ = func.arrstatmask(im, mask)
         mom = func.momanalmask_gaus(isl.image-isl.islmean, isl.mask_active, 0, 1.0, True)
         if npeak <= 1:
             g = (float(maxv), int(round(mom[1])), int(round(mom[2])), mom[3]/fwsig,
@@ -693,7 +692,7 @@ class Op_gausfit(Op):
                 xf, yf = coords[i][0], coords[i][1]
                 p_ini = [im[xf, yf], xf, yf, size, size, 0.0]
                 x, y = N.indices(im.shape)
-                p, success = func.fit_gaus2d(im*invmask[i], p_ini, x, y)
+                p, _ = func.fit_gaus2d(im*invmask[i], p_ini, x, y)
                 resid = resid + func.gaus_2d(p, x, y)
                 gaul.append(p)
             resid = im - resid
